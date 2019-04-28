50b8e2023c81cd16c8b61a1549e2f497636fdd11
Giuseppe Ursino <giursino@gmail.com>
2019-04-28 16:06:49 +0200

    removed isotope plugin not used

--- js/main.js
+++ js/main.js
@@ -154,38 +154,6 @@
       }
     }
   });
-  /*----------------------------
-   isotope active
-  ------------------------------ */
-  // portfolio start
-  $(window).on("load", function() {
-    var $container = $('.awesome-project-content');
-    $container.isotope({
-      filter: '*',
-      animationOptions: {
-        duration: 750,
-        easing: 'linear',
-        queue: false
-      }
-    });
-    var pro_menu = $('.project-menu li a');
-    pro_menu.on("click", function() {
-      var pro_menu_active = $('.project-menu li a.active');
-      pro_menu_active.removeClass('active');
-      $(this).addClass('active');
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false
-        }
-      });
-      return false;
-    });
-
-  });
   //portfolio end
 
   /*---------------------
