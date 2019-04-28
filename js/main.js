9ce1fba66c1048e81c65014953657bfe170242a1
Giuseppe Ursino <giursino@gmail.com>
2019-04-28 16:04:26 +0200

    removed knob plugin unused

--- js/main.js
+++ js/main.js
@@ -189,40 +189,6 @@
   //portfolio end
 
   /*---------------------
-   Circular Bars - Knob
-  --------------------- */
-  if (typeof($.fn.knob) != 'undefined') {
-    var knob_tex = $('.knob');
-    knob_tex.each(function() {
-      var $this = $(this),
-        knobVal = $this.attr('data-rel');
-
-      $this.knob({
-        'draw': function() {
-          $(this.i).val(this.cv + '%')
-        }
-      });
-
-      $this.appear(function() {
-        $({
-          value: 0
-        }).animate({
-          value: knobVal
-        }, {
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.val(Math.ceil(this.value)).trigger('change');
-          }
-        });
-      }, {
-        accX: 0,
-        accY: -150
-      });
-    });
-  }
-
-  /*---------------------
    Google Maps loader
   --------------------- */
   $(window).on('load', function(){
